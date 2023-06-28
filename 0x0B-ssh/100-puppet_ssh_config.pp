753aefd9eaa4826dc1eadd86863ef45031103056
Mikelonia <onajitemike4real@gmail.com>
2023-06-28 02:40:10 -0700

    Advanced Task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# using the puppet to make configurations to my file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
