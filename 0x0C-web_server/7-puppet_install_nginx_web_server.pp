fa9772896ff089b74c7b62e9cee7691d7e1ac02c
Mikelonia <onajitemike4real@gmail.com>
2023-07-14 15:57:54 -0700

    Removal of trailing space

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,12 @@
 package { 'nginx':
   ensure   => installed,
-  provider => 'apt',
 }
 
 file_line { 'install':
   ensure  => 'present',
-  path	  => 'etc/nginx/sites-enabled/default',
+  path	  => '/etc/nginx/sites-enabled/default',
   after	  => 'listen 80 default_server;',
-  line 	  => 'rewrite ^/rerdirect_me https://www.github.com/mikelonia permanent;',
+  line 	  => 'rewrite ^/redirect_me https://www.github.com/mikelonia permanent;',
 }
 file { '/var/www/html/index.html':
   content => 'Hello world!',
